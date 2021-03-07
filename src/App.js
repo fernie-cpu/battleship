9b7e451900f951f002267e60aefdb63db2d0dd7b
fernie-cpu <yoloisjustanick@gmail.com>
2021-03-07 19:00:42 -0300

    create factory files

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React from 'react';
 import Ship from './components/Ship';
 
 function App() {
-  return <div className='App'>{console.log(Ship(5, [3, 2], false))}</div>;
+  return (
+    <div className='App'>
+      <h2>still no clue what you're doing, i see</h2>
+    </div>
+  );
 }
 
 export default App;
