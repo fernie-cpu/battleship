604da71aee8ddd4fa4a84b70ddf566ab3b93f046
fernie-cpu <yoloisjustanick@gmail.com>
2021-03-13 17:07:29 -0300

    show gameboard when click play

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import GameContainer from './components/GameContainer';
 import Controls from './components/Controls';
 
 const App = () => {
+  const [restart, setRestart] = useState(false);
+  const [isGame, setIsGame] = useState(false);
+  const [showControls, setShowControls] = useState(false);
+
   return (
     <div className='App'>
       <h1>still no clue what you're doing, i see</h1>
-      <GameContainer />
-      <Controls />
+      {showControls ? (
+        <Controls
+          isGame={isGame}
+          setIsGame={setIsGame}
+          setRestart={setRestart}
+        />
+      ) : null}
+      <GameContainer
+        restart={restart}
+        setRestart={setRestart}
+        isGame={isGame}
+        setIsGame={setIsGame}
+        setShowControls={setShowControls}
+      />
     </div>
   );
 };
